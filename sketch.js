f132393f5f5b99ff8e6ee5da2e72fd2f51ff2f21
Lorraine Li <45266509+lorrrr@users.noreply.github.com>
2020-04-19 07:12:31 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 
-var diag = [0, 1, 2, 3, 4, 5, 6, 7, 7,7,7,7,7,7,7,7, 6, 5, 4, 3, 2, 1];
+var diag = [0, 1, 2, 3, 4, 5, 6, 7,8,9, 9,9,9,9,9,9,9,9,9,9,8,7, 6, 5, 4, 3, 2, 1];
 var diagCount = 0;
 var rectSize;
 var gutter;
@@ -25,14 +25,14 @@ function preload(){
 
 function setup() {
 
-  var canvas = createCanvas(windowWidth, windowWidth / 15 * 7);
+  var canvas = createCanvas(windowWidth, windowWidth / 15 * 9);
   canvas.parent('sketch-holder');
   canvas.style('display', 'block');
 
   background(255);
   noStroke();
   fill(255);
- rect(0,0,windowWidth, windowWidth / 15 * 7);
+ rect(0,0,windowWidth, windowWidth / 15 * 9);
  
   
   rectSize = windowWidth / 15;
@@ -66,7 +66,7 @@ function draw() {
   var j;
   if (diagCount>=22){
   for (i = 0; i < columns; i++) {
-    for (j = 0; j < 7; j++) {
+    for (j = 0; j < 9; j++) {
       if ((abs(pmouseX-mouseX)>1) && (abs(pmouseY-mouseY)>1))
       if (mouseX >= i * (rectSize + gutter) && mouseX <= i * (rectSize + gutter) + rectSize) {
         if (mouseY >= j * (rectSize + gutter) && mouseY <= j * (rectSize + gutter) + rectSize) {
@@ -95,7 +95,7 @@ function draw() {
   function windowResized() {
     diagCount = 0;
     background(255);
-    resizeCanvas(windowWidth, windowWidth / 15 * 7);
+    resizeCanvas(windowWidth, windowWidth / 15 * 9);
     rectSize = windowWidth / columns;
     gutter = rectSize * gutterRatio;
     rectSize = rectSize * (1 - gutterRatio);
